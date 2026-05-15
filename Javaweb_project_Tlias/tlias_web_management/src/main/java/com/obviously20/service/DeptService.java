5919a0c2196ed0e1a489a9600ff022d8cf2d32fc
obviously20 <1317418062@qq.com>
2026-05-15 20:15:49 +0800

    javaweb_backend_learning_Tlias_project_commit1

---
+++ Javaweb_project_Tlias/tlias_web_management/src/main/java/com/obviously20/service/DeptService.java
@@ -0,0 +1,4 @@
+package com.obviously20.service;
+
+public interface DeptService {
+}
---
+++ Javaweb_project_Tlias/tlias_web_management/src/main/java/com/obviously20/service/impl/DeptServiceImpl.java
@@ -0,0 +1,8 @@
+package com.obviously20.service.impl;
+
+import com.obviously20.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+}
